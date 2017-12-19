b1479af7a8405f39164c780f378635115cd09766
Satyam <satyam7991@gmail.com>
2017-12-19 22:22:53 +0530

    sengrid setup

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
 require('./models/user');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
